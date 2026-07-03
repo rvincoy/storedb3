a9e4065d2abf89b1fa75968c6d9b69034183c2b3
NateTheUnavailable <nate@stevenogardner.com>
2026-07-02 18:34:43 -0600

    working user system?

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Store DB API',
     description: 'Store DB API documentation'
   },
-  host: 'storedb-wyw9.onrender.com',
-  schemes: ['https'],
+  host: 'localhost:8080',
+  schemes: ['http'],
   components: {
     schemas: {
       Products: {
