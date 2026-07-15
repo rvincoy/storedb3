260f8132ddb5ae19f0640180c7aa878e73e167c4
mkmcgrath <matthew.k.mcgrath3@gmail.com>
2026-07-15 16:54:08 -0700

    fix swagger annotations

--- routes/ledgers.js
+++ routes/ledgers.js
@@ -3,10 +3,19 @@ const router = express.Router();
 const ledgersController = require('../controllers/ledgers');
 const { requireJWT } = require('../middleware/auth');
 
-router.get('/', ledgersController.getAll);
-router.get('/:id', ledgersController.getSingle);
-router.post('/', requireJWT, ledgersController.createLedger);
-router.put('/:id', requireJWT, ledgersController.updateLedger);
-router.delete('/:id', requireJWT, ledgersController.deleteLedger);
+router.get('/', /* #swagger.description = 'Gets all ledgers from the database' */ ledgersController.getAll);
+router.get('/:id', /* #swagger.description = 'Gets a specific ledger entry by ID' */ ledgersController.getSingle);
+router.post('/', requireJWT, /*
+    #swagger.description = 'Creates a new ledger entry'
+    #swagger.security = [{ "bearerAuth": [] }]
+*/ ledgersController.createLedger);
+router.put('/:id', requireJWT, /*
+    #swagger.description = 'Updates a specific ledger entry by ID'
+    #swagger.security = [{ "bearerAuth": [] }]
+*/ ledgersController.updateLedger);
+router.delete('/:id', requireJWT, /*
+    #swagger.description = 'Deletes a specific ledger entry by ID'
+    #swagger.security = [{ "bearerAuth": [] }]
+*/ ledgersController.deleteLedger);
 
 module.exports = router;
